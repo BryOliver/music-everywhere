3e60f953e7445544e2fdb8e20f9ad5682c758af1
Bryan Oliveira <bryan.oliver999@gmail.com>
2019-11-19 20:50:26 -0300

    #06: Modofiquei o modelo Playlist para adicionar automaticamente o usuário logado

--- me/core/admin.py
+++ me/core/admin.py
@@ -7,5 +7,12 @@ from .models import Music, Playlist
 class MusicAdmin(admin.ModelAdmin):
     list_display = ['title', 'owner', 'album']
 
+
+class PlaylistAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name',)}
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
 admin.site.register(Music, MusicAdmin)
-admin.site.register(Playlist)
+admin.site.register(Playlist, PlaylistAdmin)
--- me/core/models.py
+++ me/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,7 +8,7 @@ class Music(models.Model):
     album = models.CharField('Album', max_length=200)
     owner = models.CharField('Criador', max_length=200)
     date = models.DateField('Data de publicação')
-    arq = models.FileField('Arquivo da música')
+    arq = models.FileField('Arquivo da música', blank=True, upload_to='arquivos/musicas')
 
     def __str__(self):
         return self.title
@@ -18,9 +19,13 @@ class Music(models.Model):
 
 class Playlist(models.Model):
     slug = models.SlugField('Link', max_length=200)
-    #musics = models.ManyToManyField(Music)
+    musics = models.ManyToManyField(Music)
     name = models.CharField('Nome', max_length=200)
-    blog = models.ForeignKey(Music, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True
+    )
 
     def __str__(self):
         return self.name
