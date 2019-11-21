f5ddc6f1b62b786536be0d4cea180d37cf8b1f92
Bryan Oliveira <bryan.oliver999@gmail.com>
2019-11-21 08:32:09 -0300

    #07: Adicionei novas classes no models(album e artistas)

--- me/core/models.py
+++ me/core/models.py
@@ -2,11 +2,41 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Singer(models.Model):
+    name = models.CharField('Nome do Artista', max_length=250)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Artista'
+        verbose_name_plural = 'Artistas'
+        
+class Album(models.Model):
+    title = models.CharField('Nome do Album', max_length=250)
+    singer = models.ForeignKey(
+        Singer,
+        on_delete=models.CASCADE,
+        related_name='Autor'
+    )
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Album'
+        verbose_name_plural = 'Albuns'
 
 class Music(models.Model):
     title = models.CharField('Título', max_length = 200)
-    album = models.CharField('Album', max_length=200)
-    owner = models.CharField('Criador', max_length=200)
+    singer = models.ForeignKey(
+        Singer,
+        on_delete=models.CASCADE, 
+        related_name='Autor'
+    )
+    album = models.ForeignKey(
+        Album,
+        on_delete=models.CASCADE,
+        related_name='Album'
+    )
     date = models.DateField('Data de publicação')
     arq = models.FileField('Arquivo da música', blank=True, upload_to='arquivos/musicas')
 
@@ -18,14 +48,17 @@ class Music(models.Model):
         verbose_name_plural = 'Músicas'
 
 class Playlist(models.Model):
-    slug = models.SlugField('Link', max_length=200)
-    musics = models.ManyToManyField(Music)
     name = models.CharField('Nome', max_length=200)
+    slug = models.SlugField('Link', max_length=200)
+    music = models.ManyToManyField(Music)
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        null=True
+        null=True,
+        related_name='Criado'
     )
+    criacao = models.DateField('Data de criação', auto_now_add=True)
+    modificacao = models.DateField('Atualizado em', auto_now=True)
 
     def __str__(self):
         return self.name
